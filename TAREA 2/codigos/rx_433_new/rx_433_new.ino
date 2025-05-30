a979165cebe29f8d13dbb134f9de224847e7066f
Panqueque <108034714+Panquequ3@users.noreply.github.com>
2025-05-30 15:28:49 -0400

    añadido codigo RR10

--- TAREA 2/codigos/rx_433_new/rx_433_new.ino
+++ TAREA 2/codigos/rx_433_new/rx_433_new.ino
@@ -15,6 +15,20 @@ pin 15 5v
 
 #include <VirtualWire.h>
 
+
+int verde = 4;
+int const MESSAGE_TAM = 3; //Bytes
+int const ID = 2;
+int const ID_RECEPTOR = 2;
+int const TAM = 6;
+
+String get_message(char* message){
+  int emisor = (int)message[1];
+  if(emisor != ID_RECEPTOR) return "NaN";
+  int receptor = (int)message[2];
+  if(receptor != ID) return "NaN";
+}
+
 void setup(){
     Serial.begin(9600);
     Serial.println("Configurando Recepcion");
@@ -22,6 +36,8 @@ void setup(){
     vw_setup(2000);
     vw_set_rx_pin(2);
     vw_rx_start();
+
+    pinMode(verde, OUTPUT);
 }
 
 void loop(){
@@ -29,14 +45,19 @@ void loop(){
     uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
     if (vw_get_message(buf, &buflen)){
-        char m[5]="H";
-	int i;
-        digitalWrite(13, true);
-	for (i = 0; i <= 5; i++){
-	    m[i] = (char)buf[i];
-	}
-        Serial.print("Mensaje Recibido = ");
-        Serial.println(m);
-        digitalWrite(13, false);
-    }
+        if((int)buf[1]==ID_RECEPTOR && (int)buf[2]==ID){
+          char m[TAM-3]="H";
+          int i;
+          digitalWrite(13, true);
+          digitalWrite(verde, HIGH);
+          for (i = 0; i <= TAM-3; i++){
+              m[i] = (char)buf[i+3];
+          }
+          Serial.print("Mensaje Recibido = ");
+          Serial.println(m);
+          digitalWrite(13, false);
+          delay(500);
+          digitalWrite(verde, LOW);
+      }
+    } 
 }
