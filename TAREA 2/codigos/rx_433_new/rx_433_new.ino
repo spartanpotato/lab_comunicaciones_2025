51815f8df5465a8efa57ba34f6d786023e35c1c5
maDummy47 <alvaro.burgosandrade@gmail.com>
2025-06-04 12:14:42 -0400

    main & receptor

--- TAREA 2/codigos/rx_433_new/rx_433_new.ino
+++ TAREA 2/codigos/rx_433_new/rx_433_new.ino
@@ -19,28 +19,43 @@ int verde = 4;
 int const MESSAGE_TAM = 3; //Bytes
 int const ID = 2;
 int const ID_RECEPTOR = 2;
-int const TAM = 6;
+int const TAM = 7; //Bytes totales utilizados en el mensaje
 int const TAM_ARRAY = (32*32)/24 + 1;
 String messages[TAM_ARRAY];
-String image_total = "";
 
 
 // Seq  ID  ID_Receptor Message checksum
 // []   []      []      [][][]     []
 
+bool is_full(){
+  for(String elem: messages){
+    if(elem == "H"){
+      return false;
+    }
+  }
+  return true;
+}
 
-// 1 - 2 - 2 - "hola" - checksum
-// 2 - 2 - 2 - "como" - checksum
-// 3 - 2 - 2 - "estas" - checksum
-
-//     1         2       3 
-// [ "hola" , "como", "estas"]
-
-
+bool check_crc8(uint8_t *d, uint8_t crc_recv){
+  uint8_t crc_calc = crc8(d, TAM-1); // Calcula CRC de los primeros 6 bytes
+  if(crc_recv == crc_calc){
+    return true;
+  }
+  else{
+    Serial.print("CRC no valido: ");
+    Serial.print(crc_recv);
+    Serial.print(" != ");
+    Serial.println(crc_calc);
+    return false;
+  }
+}
 
 uint8_t crc8(const uint8_t *d, uint8_t n) {
   uint8_t crc = 0x00;
-  while(n--) { crc ^= *d++; for(uint8_t i=0; i<8; i++) crc = (crc<<1)^((crc&0x80)?0x07:0); }
+  while(n--) { 
+    crc ^= *d++; 
+    for(uint8_t i=0; i<TAM; i++) crc = (crc<<1)^((crc&0x80)?0x07:0);
+  }
   return crc;
 }
 
@@ -61,6 +76,11 @@ void get_message(uint8_t* message){
   for(int i = 3; i<=TAM-1; i++){
     image+= get_byte(message[i]);
   }
+  if(sequence < 0 || sequence >= TAM_ARRAY){
+    Serial.print("Secuencia fuera de rango: ");
+    Serial.println(sequence);
+    return;
+  }
   messages[sequence] = image;
   // return image;
 }
@@ -78,71 +98,47 @@ void setup(){
     }
 }
 
-void loop(){
-    int mensajes_recv = 0;
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
-    while(mensajes_recv < TAM_ARRAY){
-      if (vw_get_message(buf, &buflen)){
-          if((int)buf[1]==ID_RECEPTOR && (int)buf[2]==ID){
-            /*uint8_t crc8_recv = (uint8_t)buf[6]
-            uint8_t crc8_calc = crc8(buf, 6);
-            if (crc8_recv != crc8_calc){
-              Serial.print("no paso prueba crc8");
-            }*/
-            digitalWrite(13, true);
-            digitalWrite(verde, HIGH);
-            get_message(buf);
-            if(messages[(int)buf[0]] != "H"){
-              mensajes_recv++;
-            }
-            Serial.print(mensajes_recv);
-            digitalWrite(13, false);
-            delay(500);
-            digitalWrite(verde, LOW);
-        }
-      } 
+void loop() {
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  // Espera a recibir un mensaje
+  if (vw_get_message(buf, &buflen)) {
+    // Verifica si el mensaje es el esperado
+    if ((int)buf[1] == ID_RECEPTOR && (int)buf[2] == ID) {
+      // Verifica si el mensaje mantiene el checksum
+      if (check_crc8(buf, buf[7])) {
+        digitalWrite(13, true);
+        digitalWrite(verde, HIGH);
+        get_message(buf);
+        digitalWrite(13, false);
+        delay(500);
+        digitalWrite(verde, LOW);
+      }
     }
-    if(mensajes_recv == TAM_ARRAY){
-      Serial.println("Imagen:");
-      int cont = 0;
-      for (String elem : messages){
-        for (char c : elem) { 
-          if(c == '0'){
-              Serial.print("!");
-              cont++;
-            }
-            else{
-              Serial.print("?");
-              cont++;
-            }
-          if((cont) == 32){
-            Serial.println("");
-            cont = 0;
-          }     
+  }
+
+  //Imprime la imagen
+  if (is_full()) {
+    Serial.println("Imagen:");
+    int cont = 0;
+    for (String elem : messages) {
+      for (char c : elem) {
+        if (c == '0') {
+          Serial.print("# ");
+          cont++;
+        } else {
+          Serial.print("  ");
+          cont++;
+        }
+        if ((cont) == 32) {
+          Serial.println("");
+          cont = 0;
         }
       }
-      mensajes_recv = 0;
-      //Aqui deberia vaciar ;;
-      for (int i = 0; i < TAM_ARRAY; i++) {
-        messages[i] = "H"; // Asigna "H" a cada posición
-      }
-      //
-      // for(int j = 0; j<16*16; j++){
-      //   if(image_total[j]=='0'){
-      //     Serial.print("!");
-      //   }
-      //   else{
-      //     Serial.print("?");
-      //   }
-      //   if((j+1)%16==0){
-      //     Serial.println("");
-      //   }
-      // }
-
-
-      // Serial.println(image_total);
-      // image_total = "";
     }
+    Serial.println("");
+    for (int i = 0; i < TAM_ARRAY; i++) {
+      messages[i] = "H";
+    }
+  }
 }
